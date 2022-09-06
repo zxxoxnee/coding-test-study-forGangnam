e1173411e5fd3d046ff7b96c936e08a2c420e3e4
freemjstudio <mjwoo0@naver.com>
2022-09-06 16:39:27 +0900

    16953 우민지

--- 2022년 9월/6일_/우민지.py
+++ 2022년 9월/6일_/우민지.py
@@ -1,2 +1,18 @@
 # 16953 우민지
 
+a, b = map(int, input().split())
+count = 1
+while True:
+    if b == a: # A->B 가능한 경우
+        break
+    elif (b < a) or (b%10 != 1 and b%2 != 0): # A->B 불가능한 경우 예외처리
+        count = -1
+        break
+    else:
+        if b%10==1:
+            b = b//10
+            count += 1
+        else:
+            b = b//2
+            count += 1
+print(count)
