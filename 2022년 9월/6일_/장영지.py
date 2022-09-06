0e06417e69eadde062dacab655260b3d67bc17f7
youngji <youngji.jang804@gmail.com>
2022-09-06 12:18:34 +0900

    0906 장영지

---
+++ 2022년 9월/6일_/장영지.py
@@ -0,0 +1,20 @@
+import sys
+
+a,b = map(int,sys.stdin.readline().split())
+count = 0
+while b > a :
+  while str(b)[-1] == '1' :
+    b = str(b)
+    b = int(b[:len(b)-1])
+    count += 1
+  if b % 2 != 0 : 
+    print(-1)
+    exit(0)
+  b = b//2
+  count += 1
+
+# print(a,b)
+if a == b : print(count+1)
+else : print(-1)
+
+### 반례 찾는 중ㅜㅜㅜ
