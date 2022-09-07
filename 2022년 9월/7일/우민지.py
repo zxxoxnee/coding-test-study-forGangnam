5bebebd7dbfa7438cf6930a5775234c4ddb58259
freemjstudio <mjwoo0@naver.com>
2022-09-07 23:51:27 +0900

    우민지 풀이

--- 2022년 9월/7일/우민지.py
+++ 2022년 9월/7일/우민지.py
@@ -5,3 +5,12 @@ array = [[0]*n for _ in range(n)]
 for _ in range(n*n):
     students.append(map(int, input().split()))
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# 학생 자리 배치
+for i in range(n*n):
+
+# 학생의 만족도 총 합
+result = 0
+print(result)
