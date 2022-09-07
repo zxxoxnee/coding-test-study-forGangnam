9531c70dc054449747f31853d99d90bfd6ca587d
freemjstudio <mjwoo0@naver.com>
2022-09-08 03:20:27 +0900

    우민지-21608

--- 2022년 9월/7일/우민지.py
+++ 2022년 9월/7일/우민지.py
@@ -1,35 +1,48 @@
 n = int(input())
-students = [] # 학생에 대한 정보
+
 array = [[0]*n for _ in range(n)]
-
-for _ in range(n*n):
-    students.append(map(int, input().split()))
-
+student_dict = {} # 순 있는 딕셔너리 ?
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 result = 0 # 만족도
-# 학생 자리 배치
-for student in students:
-    temp = []
+for i in range(n*n):
+    data = list(map(int, input().split()))
+    student_dict[data[0]] = data[1:]
+    max_x, max_y = 0, 0
+    max_like = -1
+    max_empty = -1
+
     for x in range(n):
         for y in range(n):
-            empty = 0
-            like = 0  # 좋아하는 학생
-            for k in range(4):
-                nx = x + dx[k]
-                ny = y + dy[k]
-                if 0 <= nx < n*n and 0 <= ny < n*n:
-                    if array[nx][ny] == 0:
-                        empty += 1
-                    elif array[nx][ny] in student[1:]:
-                        like += 1
-            temp.append((empty, like, x, y))
+            if array[x][y] == 0: # 빈자리인지 우선 체크
+                like = 0
+                empty = 0
+                for k in range(4):
+                    nx = x + dx[k]
+                    ny = y + dy[k]
+                    if 0 <= nx < n and 0 <= ny < n:
+                        if array[nx][ny] == 0: # 빈자리
+                            empty += 1
+                        if array[nx][ny] in student_dict[data[0]]: # 좋아하는 학생
+                            like += 1
+                if max_like < like or (max_empty < empty and max_like == like):
+                    max_x, max_y = x, y
+                    max_like = like
+                    max_empty = empty
+    array[max_x][max_y] = data[0]
 
-    temp.sort()
-    nx, ny = temp[0][2], temp[0][3]
-    array[nx][ny] = student[0] # 현재 배치하는 학생
-    result += temp[0][1]
+for i in range(n):
+    for j in range(n):
+        like = 0
+        like_student = student_dict[array[i][j]]
+        for k in range(4):
+            nx = i + dx[k]
+            ny = j + dy[k]
+            if 0 <= nx < n and 0 <= ny < n:
+                if array[nx][ny] in like_student:
+                    like += 1
+        if like > 0:
+            result += 10**(like-1)
 
 print(result)
 
