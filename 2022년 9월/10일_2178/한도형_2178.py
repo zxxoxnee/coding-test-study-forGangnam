2aacede1fde29a576eefb23080cb0a7462415580
2019065614 <dohan2038@gmail.com>
2022-09-10 20:52:19 +0900

    dohyeong_2178

---
+++ 2022년 9월/10일_2178/한도형_2178.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m = map(int, input().split())
+# print(n,m)
+graph = [list(map(int, input())) for _ in range(n)]
+# print(graph)
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx >= n or nx < 0 or ny >= m or ny < 0:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n - 1][m - 1]
+
+
+print(bfs(0,0))
