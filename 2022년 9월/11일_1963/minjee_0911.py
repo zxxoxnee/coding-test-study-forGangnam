bbdfec430f26ae987fbfbd6f50cc271c90cea039
freemjstudio <mjwoo0@naver.com>
2022-09-10 14:25:35 +0900

    우민지 0911

--- 2022년 9월/11일_1963/minjee_0911.py
+++ 2022년 9월/11일_1963/minjee_0911.py
@@ -1,18 +1,19 @@
 # https://www.acmicpc.net/problem/1963
 from collections import deque
-# 에라스토스의 체 -> 소수 판별
-array = [True] * (10000)
-for i in range(10000):
-    if array[i]:
-        j = 2
-        while i*j < 10000:
-            array[i*j] = False
-            j += 1
+import sys
+# 에라토스테네의 체 -> 소수 판별
+array = [True] * 10000
+
+for x in range(2, 100):
+    if array[x]:
+        y = 2
+        for y in range(x*2, 10000, x):
+            array[y] = False
 
 def solve(a, b):
     queue = deque()
     queue.append((a, 0)) # (현재 비밀번호, count)
-    visited = [False] * 10001 # 1000 ~ 9999 표시
+    visited = [False] * 10000 # 1000 ~ 9999 표시
     visited[a] = True
     while queue:
         num, count = queue.popleft()
@@ -24,17 +25,17 @@ def solve(a, b):
         str_num = str(num)
         for i in range(4):
             for j in range(10): # 0 ~ 9
-                temp = int(str_num[:i] + str(j) + str_num[i:])
+                temp = int(str_num[:i] + str(j) + str_num[i+1:])
                 if visited[temp] == False and array[temp] == True:
                     visited[temp] = True
                     queue.append((temp, count+1))
 
-t = int(input())
+t = int(sys.stdin.readline())
 for _ in range(t):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     result = solve(a, b)
-    if result == None:
-        print("Impossible")
-    else:
+    if result != None:
         print(result)
+    else:
+        print("Impossible")
 
