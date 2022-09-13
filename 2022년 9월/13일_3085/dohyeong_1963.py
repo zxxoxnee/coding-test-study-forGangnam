d2bbf4b29b6268683c20744c33fe970e3a21a4d0
unknown <dohan2038@gmail.com>
2022-09-14 01:13:56 +0900

    dohyeong_1963

--- 2022년 9월/13일_3085/dohyeong_1963.py
+++ 2022년 9월/13일_3085/dohyeong_1963.py
@@ -14,9 +14,21 @@ def check():
                 cnt = 1
         cnt = 1
         for j in range(0,n-1):
-            if graph[j][i] == graph[j+1][i]
+            if graph[j][i] == graph[j+1][i]:
                 cnt += 1
                 ans = max(ans, cnt)
             else:
                 cnt = 1
+for i in range(n):
+    for j in range(n):
+        if j+1 < n:
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
+            check()
+            graph[i][j], graph[i][j+1] = graph[i][j+1], graph[i][j]
+
+        if i+1 < n:
+            graph[i][j], graph[i+1][j] = graph[i+1][j], graph[i][j]
+            check()
+            graph[i][j], graph[i + 1][j] = graph[i + 1][j], graph[i][j]
+print(ans)
 
