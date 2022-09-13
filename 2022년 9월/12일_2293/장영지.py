95a1860acfa1f05b96cf787a9f74db2cdcf3bcc5
youngji <youngji.jang804@gmail.com>
2022-09-13 22:16:26 +0900

    0913 영지

--- 2022년 9월/12일_2293/장영지.py
+++ 2022년 9월/12일_2293/장영지.py
@@ -4,12 +4,14 @@ n,k = map(int,sys.stdin.readline().split())
 coin = []
 for _ in range(n) :
   a = int(sys.stdin.readline())
-  coin.append(a)
+  if a <= k :
+    coin.append(a)
 
-dp = [0 for _ in range(k+1)]
-dp[0] = 1
+dp = [0 for n in range(k+1)] # 가치의 합이 n이 되는 경우의 수
+# dp[0] = 1
 
 for c in coin :
+  dp[c] += 1
   for j in range(c,k+1) :
     if j-c >= 0 :
       dp[j] += dp[j-c]
