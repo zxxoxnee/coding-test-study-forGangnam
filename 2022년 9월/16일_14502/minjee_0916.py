94d9b0e75b6edec954c6eab991b9a051e878ad73
freemjstudio <mjwoo0@naver.com>
2022-09-16 17:13:11 +0900

    minjee_0916

--- 2022년 9월/16일_14502/minjee_0916.py
+++ 2022년 9월/16일_14502/minjee_0916.py
@@ -1,5 +1,4 @@
 # 14502 연구소
-from itertools import combinations
 import copy
 
 n, m = map(int, input().split())
@@ -7,8 +6,7 @@ array = []
 temp = [[0] *m for _ in range(n)] # 바이러스 퍼지는거 확인하기 위해서
 
 for i in range(n):
-    data = list(map(int, input().split()))
-    array.append(data)
+    array.append(list(map(int, input().split())))
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
@@ -21,6 +19,7 @@ def virus(x, y):
         if 0 <= nx < n and 0 <= ny < m:
             if temp[nx][ny] == 0:
                 temp[nx][ny] = 2 # new virus
+                virus(nx, ny)
 
 answer = 0
 
@@ -36,13 +35,16 @@ def check():
 def block(count):
     global answer
     if count == 3: # 벽 세개인 경우 검증하기
-        temp = copy.deepcopy(array)
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = array[i][j]
         for i in range(n):
             for j in range(m):
                 if temp[i][j] == 2:
                     virus(i, j)
 
         answer = max(answer, check()) # 최대값으로 갱신하기
+        return
 
     # 벽 개수가 아직 3개 미만인 경우 -> 벽을 추가한다 , 백트래킹
     for i in range(n):
