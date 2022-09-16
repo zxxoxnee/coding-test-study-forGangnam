15e0417ea4f95f78c2f9b0d7283d9d03c72639b7
freemjstudio <mjwoo0@naver.com>
2022-09-16 17:08:12 +0900

    minjee_0916

--- 2022년 9월/16일_14502/minjee_0916.py
+++ 2022년 9월/16일_14502/minjee_0916.py
@@ -1,10 +1,10 @@
 # 14502 연구소
 from itertools import combinations
-from collections import deque
 import copy
 
 n, m = map(int, input().split())
 array = []
+temp = [[0] *m for _ in range(n)] # 바이러스 퍼지는거 확인하기 위해서
 
 for i in range(n):
     data = list(map(int, input().split()))
@@ -15,18 +15,44 @@ dy = [-1, 1, 0, 0]
 
 # 바이러스 퍼뜨리는 함수
 def virus(x, y):
-    temp = copy.deepcopy(array)
-    queue = deque()
-    queue.append((x, y))
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m:
+            if temp[nx][ny] == 0:
+                temp[nx][ny] = 2 # new virus
 
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if temp[nx][ny] == 0:
-                    temp[nx][ny] = 2
-                    queue.append((nx, ny))
-    return temp
+answer = 0
 
+def check():
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                result += 1
+    return result
+
+# 벽 3개
+def block(count):
+    global answer
+    if count == 3: # 벽 세개인 경우 검증하기
+        temp = copy.deepcopy(array)
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i, j)
+
+        answer = max(answer, check()) # 최대값으로 갱신하기
+
+    # 벽 개수가 아직 3개 미만인 경우 -> 벽을 추가한다 , 백트래킹
+    for i in range(n):
+        for j in range(m):
+            if array[i][j] == 0:
+                array[i][j] = 1
+                count += 1
+                block(count)
+                array[i][j] = 0 # 다시 back
+                count -= 1
+
+block(0)
+print(answer)
