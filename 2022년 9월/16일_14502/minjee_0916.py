a4c3fc75179ff9c2e70d9244cc69d9ab970b734d
freemjstudio <mjwoo0@naver.com>
2022-09-16 17:15:42 +0900

    minjee_0916

--- 2022년 9월/16일_14502/minjee_0916.py
+++ 2022년 9월/16일_14502/minjee_0916.py
@@ -1,5 +1,6 @@
 # 14502 연구소
 import copy
+from collections import deque
 
 n, m = map(int, input().split())
 array = []
@@ -13,13 +14,17 @@ dy = [-1, 1, 0, 0]
 
 # 바이러스 퍼뜨리는 함수
 def virus(x, y):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2 # new virus
-                virus(nx, ny)
+    queue = deque()
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if temp[nx][ny] == 0:
+                    temp[nx][ny] = 2 # new virus
+                    queue.append((nx, ny))
 
 answer = 0
 
