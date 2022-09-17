a89d85677000cb4f2159759ff147ad042833c7fc
2019065614 <dohan2038@gmail.com>
2022-09-17 22:29:09 +0900

    dohyeong_14888

--- 2022년 9월/16일_14502/dohyeong_14502.py
+++ 2022년 9월/16일_14502/dohyeong_14502.py
@@ -1,8 +1,10 @@
 n, m = map(int, input().split())
 graph = []
 for _ in range(n):
-    graph.append(list(map(int,input().split())))
+    graph.append(list(map(int, input().split())))
+# print(graph)
 res = [[0] * m for _ in range(n)]
+# print(res)
 ans = 0
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
@@ -12,16 +14,25 @@ dy = [0,0,-1,1]
 # 바이러스 시작점에서 퍼지는 메서드
 def spread(x,y):
     for i in range(4):
-        nx, ny = x + dx(i), y + dy(i)
-        if 0<=nx<n and 0<=ny<m:
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny <m:
             if res[nx][ny] == 0:
                 res[nx][ny] = 2
                 spread(nx,ny)
 
+# 안전영역 체크
+def check():
+    check = 0
+    for i in range(n):
+        for j in range(m):
+            if res[i][j] == 0:
+                check += 1
+    return check
+
 def dfs(cnt):
     global ans
     # 종료 조건 울타리 3개
-    if cnt ==3:
+    if cnt == 3:
         for i in range(n):
             for j in range(m):
                 res[i][j] = graph[i][j]
@@ -29,14 +40,9 @@ def dfs(cnt):
             for j in range(m):
                 if res[i][j] == 2:
                     spread(i,j)
-
-        num = 0
-        for i in range(n):
-            for j in range(m):
-                if res[i][j] == 0:
-                    num += 1
-        ans = max(ans, num)
+        ans = max(ans, check())
         return
+
     # 울타리 설치 dfs로 구현
     for i in range(n):
         for j in range(m):
@@ -44,4 +50,9 @@ def dfs(cnt):
                 graph[i][j] = 1
                 cnt += 1
                 dfs(cnt)
+                graph[i][j] = 0
+                cnt -= 1
+
+dfs(0)
+print(ans)
 
