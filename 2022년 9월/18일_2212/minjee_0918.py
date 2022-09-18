7633a309caf1fc2850245b4c8672a2200bb7d343
freemjstudio <mjwoo0@naver.com>
2022-09-18 16:23:00 +0900

    minjee_0918

--- 2022년 9월/18일_2212/minjee_0918.py
+++ 2022년 9월/18일_2212/minjee_0918.py
@@ -1,5 +1,16 @@
 # 2212 센서
-n = int(input())
-k = int(input())
+n = int(input()) # sensor 개수
+k = int(input()) # 집중국 개수
 sensor = list(map(int, input().split()))
-sensor.sort()
+
+if n <= k:
+    print(0)
+else:
+    sensor.sort()
+    distance = [] # 각 센서끼리의 거리의 차를 저장하는 배열
+    for i in range(n-1):
+        distance.append(sensor[i+1]-sensor[i])
+    distance.sort(reverse=True)
+    # 가장 큰 값 k-1 개를 뺀
+    print(sum(distance[k-1:]))
+
