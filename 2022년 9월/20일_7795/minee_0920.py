3ac165a222597ade4a32360bc882799ac06979ab
freemjstudio <mjwoo0@naver.com>
2022-09-20 22:47:01 +0900

    minjee 0920

--- 2022년 9월/20일_7795/minee_0920.py
+++ 2022년 9월/20일_7795/minee_0920.py
@@ -1,1 +1,19 @@
 # 7795
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    array_a = list(map(int, input().split()))
+    array_b = list(map(int, input().split()))
+    # a가 b보다 큰 쌍의 개수
+    array_a.sort(reverse=True)
+    array_b.sort()
+    count = 0
+    for i in range(n):
+        if i > 0 and array_a[i] == array_a[i-1]:
+            continue
+        for j in range(m):
+            if j > 0 and array_b[j] == array_b[j-1]:
+                continue
+            if array_a[i] > array_b[j]:
+                count += 1
+    print(count)
