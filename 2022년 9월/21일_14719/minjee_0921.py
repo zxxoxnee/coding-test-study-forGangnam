3e696b046a2821a631ecafae67594f07b5c91848
freemjstudio <mjwoo0@naver.com>
2022-09-21 23:43:46 +0900

    minjee_0921

--- 2022년 9월/21일_14719/minjee_0921.py
+++ 2022년 9월/21일_14719/minjee_0921.py
@@ -1,13 +1,12 @@
 h, w = map(int, input().split())
 block = list(map(int, input().split()))
 volume = 0
-stack = []
-for i in range(w):
-    stack.append(i) # index
-    if block[stack[-1]] < block[i]:
-        height = stack.pop()
-        volume += block[]
 
-
+for i in range(1, w-1):
+    # 자기 자신의 높이보다 좌/우에 높은 것이 있는지 확인
+    left = max(block[:i])
+    right = max(block[i+1:])
+    height = min(left, right) # 둘 중에 작은 값이 기준이 된다
+    volume += height - block[i]
 
 print(volume)
