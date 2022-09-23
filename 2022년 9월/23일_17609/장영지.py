cd86b4881534d546ccba8de25e2a8821b6ac87a3
youngji <youngji.jang804@gmail.com>
2022-09-23 14:38:09 +0900

    0923

---
+++ 2022년 9월/23일_17609/장영지.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t) :
+  s = sys.stdin.readline().strip()
+  if s ==  s[::-1] :
+    print(0)
+  else :
+    for i in range(len(s)) :
+      if s.count(i) == 1 :
+        s[]
