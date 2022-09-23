6d03837032c39559a2ee8c08f042da42fd05c9e8
freemjstudio <mjwoo0@naver.com>
2022-09-23 18:25:18 +0900

    minjee 0923

--- 2022년 9월/23일_17609/minjee_0922.py
+++ 2022년 9월/23일_17609/minjee_0922.py
@@ -1,84 +1,28 @@
 n = int(input())
-for _ in range(n):
-    word = input()
-    left = 0
-    right = len(word) -1
-    similar = 0
-    check = 0 # ababbabaa 이런 케이스 검증용
-    c_similar = 0
+
+def solve(left, right):
     while left <= right:
-        print(left, right)
-        print(word[left], word[right])
         if word[left] == word[right]:
             left += 1
             right -= 1
-            print("=====")
         else:
-            if word[left+1] == word[right] and word[left] == word[right-1]:
-                # 왼쪽 땡기는 방법
-                if check == 0:
-                    i = left + 1
-                    j = right
-                    check += 1 # 왼쪽 땡기는거 체크했다는 뜻임
-                    while i <= j:
-                        if word[i] == word[j]:
-                            i += 1
-                            j -= 1
-                        else:
-                            if word[i+1] == word[j]:
-                                i += 1
-                                j -= 1
-                            else:
-                                if word[i + 1] == word[j]:
-                                    i += 1
-                                    c_similar += 1
-                                elif word[i] == word[j + 1]:
-                                    j += 1
-                                    c_similar += 1
-                                else:
-                                    c_similar += 2
-                                    break
+            # 오른쪽 문자열 제거
+            if left < right -1:
+                temp = word[:right] + word[right+1:]
+                if temp[:] == temp[::-1]:
+                    return 1
+            # 왼쪽 문자열 제거
+            if left + 1 < right:
+                temp = word[:left] + word[left+1:]
+                if temp[:] == temp[::-1]:
+                    return 1
+            return 2
+    return 0
 
-                if check == 1: # 오른쪽 땡기는거 검사할 차
-                    i = left
-                    j = right - 1
-                    while i <= j:
-                        if word[i] == word[j]:
-                            i += 1
-                            j -= 1
-                        else:
-                            if word[i+1] == word[j]:
-                                i += 1
-                                c_similar += 1
-                            elif word[i] == word[j+1]:
-                                j += 1
-                                c_similar += 1
-                            else:
-                                c_similar += 2
-                                break
+for _ in range(n):
+    word = input()
+    left = 0
+    right = len(word) - 1
+    print(solve(left, right))
 
 
-            if check == 1: #이미 검사함
-                break
-            elif word[left+1] == word[right]:
-                print("llll")
-                left += 1
-                similar += 1
-            elif word[left] == word[right-1]:
-                print("rrrrr")
-                right -= 1
-                similar += 1
-            else: # 다른 경우
-                similar += 2
-                print("hhh")
-                break
-    if check == 0: # 특이 케이스 없는 경우
-        if similar >= 2:
-            print(2)
-        else:
-            print(similar)
-    else:
-        if c_similar == 0:
-            print(1)
-        else:
-            print(2)
