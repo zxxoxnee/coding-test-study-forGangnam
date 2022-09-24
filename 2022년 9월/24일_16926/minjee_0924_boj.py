9b63bfd13464ac67ed6739b24c81d9356b171b52
freemjstudio <mjwoo0@naver.com>
2022-09-25 00:27:23 +0900

    minjee 0924

--- 2022년 9월/24일_16926/minjee_0924_boj.py
+++ 2022년 9월/24일_16926/minjee_0924_boj.py
@@ -24,13 +24,13 @@ for _ in range(r):
             value = temp
 
         for j in range(i+1, n-i): # right
-            x = n - j
-            temp = array[x][y]
+            x = n - j - 1 # -1 을 해줘야 함 왜냐면 그 이전에 좌표는 예를들어서 n = m = 4 이면 (3,3) 임
+            temp = array[x][y] # -1을 해줘야 예전값을 넣을 그 다음 칸을 가리킬 수 있음
             array[x][y] = value
             value = temp
 
         for j in range(i+1, m-i): # top
-            y = n - j
+            y = n - j - 1
             temp = array[x][y]
             array[x][y] = value
             value = temp
