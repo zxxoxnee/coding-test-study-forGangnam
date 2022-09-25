c9e68ce2bd8b157115f0d67ece4d2c7cdaad901c
freemjstudio <mjwoo0@naver.com>
2022-09-25 23:04:08 +0900

    minjee 0925

--- 2022년 9월/25일_14382/minjee_14382.py
+++ 2022년 9월/25일_14382/minjee_14382.py
@@ -1,1 +1,14 @@
-# 14382 
+# 14382
+t = int(input())
+
+for i in range(t):
+    check = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+    n = int(input())
+    temp = str(n)
+    for j in temp:
+        check[int(j)] += 1
+
+    # 확인하기
+
+
+    print("Case #",i,":")
