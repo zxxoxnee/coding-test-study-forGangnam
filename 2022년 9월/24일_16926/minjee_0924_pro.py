c363c316a53457f28ef383e420091e12966be317
freemjstudio <mjwoo0@naver.com>
2022-09-27 01:21:03 +0900

    programmers - minjee

--- 2022년 9월/24일_16926/minjee_0924_pro.py
+++ 2022년 9월/24일_16926/minjee_0924_pro.py
@@ -1,30 +1,27 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/92344
-
 def solution(board, skill):
     answer = 0
 
-    def attack(a, b, c, d, degree):
-        for i in range(a, c + 1):
-            for j in range(b, d + 1):
-                board[i][j] -= degree
-
-    def save(a, b, c, d, degree):
-        for i in range(a, c + 1):
-            for j in range(b, d + 1):
-                board[i][j] += degree
-
     n = len(board)
     m = len(board[0])
-
-    for data in skill:
-        if data[0] == 1:
-            attack(data[1], data[2], data[3], data[4], data[5])
-        else:
-            save(data[1], data[2], data[3], data[4], data[5])
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    for type, r1, c1, r2, c2, degree in skill:
+        if type == 1:
+            degree = (-1) * degree
+        dp[r1][c1] += degree
+        dp[r2 + 1][c1] -= degree
+        dp[r1][c2 + 1] -= degree
+        dp[r2 + 1][c2 + 1] += degree
+    # 누적합 계산
     for i in range(n):
         for j in range(m):
-            if board[i][j] >= 1:
+            dp[i + 1][j] += dp[i][j]
+
+    for i in range(n):
+        for j in range(m):
+            dp[i][j + 1] += dp[i][j]
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] + dp[i][j] > 0:
                 answer += 1
     return answer
-
-# 효율성을 어떻게 개선하지 ????
