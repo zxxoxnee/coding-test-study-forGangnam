a6ee235017ec3972b0486e96b80bf74663653404
freemjstudio <mjwoo0@naver.com>
2022-09-26 22:05:03 +0900

    programmers - minjee

--- 2022년 9월/26일_16987/minjee_0926_pro.py
+++ 2022년 9월/26일_16987/minjee_0926_pro.py
@@ -2,14 +2,14 @@ def solution(survey, choices):
     answer = ''
     n = len(choices)
     type = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
+    score = [0, 3, 2, 1, 0, 1, 2, 3]
     for i in range(n):
         if choices[i] == 4:
-            type[survey[i][0]] += 0
-            type[survey[i][1]] += 0
+            continue
         if choices[i] < 4:
-            type[survey[i][0]] += choices[i]
+            type[survey[i][0]] += score[choices[i]]
         if choices[i] > 4:
-            type[survey[i][1]] += (choices[i] - 4)
+            type[survey[i][1]] += score[choices[i]]
     print(type)
     if type['R'] >= type['T']:
         answer += 'R'
