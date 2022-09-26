dc28249277bd7bcbd4d55f618d7709aa0a8d9177
freemjstudio <mjwoo0@naver.com>
2022-09-26 13:38:40 +0900

    minjee 0925

--- 2022년 9월/25일_14382/minjee_14382.py
+++ 2022년 9월/25일_14382/minjee_14382.py
@@ -4,11 +4,23 @@ t = int(input())
 for i in range(t):
     check = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
     n = int(input())
-    temp = str(n)
-    for j in temp:
-        check[int(j)] += 1
+    if n == 0:
+        print("Case #", i, ": INSOMNIA")
+    else:
+        count = 1
+        while True:
+            temp = str(n)
+            for j in temp:
+                check[int(j)] = 1
+            total = 0
+            for k in check.values():
+                if k != 0:
+                    total += 1
+            if total == 10:
+                print("Case #", i, ": ",n)
+                break
+            n *= count
 
     # 확인하기
 
 
-    print("Case #",i,":")
