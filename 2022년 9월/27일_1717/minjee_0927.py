e17b2aafb0bb2074ea54ef1d87a49c5d76059963
freemjstudio <mjwoo0@naver.com>
2022-09-27 15:30:26 +0900

    minjee 0927

--- 2022년 9월/27일_1717/minjee_0927.py
+++ 2022년 9월/27일_1717/minjee_0927.py
@@ -1,33 +1,28 @@
 # 1717 백준
+import sys
+sys.setrecursionlimit(1000000)
+n, m = map(int, sys.stdin.readline().split())
+parents = [i for i in range(n+1)]
 
-n, m = map(int, input().split())
-parents = [0] * (n+1)
-
-for i in range(1, n+1):
-    parents[i] = i # 자기 자신을 부모로 설정
-
-def find(a):
-    if a == parents[a]: # 자기 자신이 루트 노드
-        return a
-    root = find(parents[a]) # 아닌 경우에는 parents[a] 의 상위 노드를 확인한다
-    parents[a] = root
+def find_parents(a):
+    if parents[a] != a: # 자기 자신이 root 노드가 아닌경우 다시 find 연산 수행하기
+        parents[a] = find_parents(parents[a])
     return parents[a]
 
 def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a == b:
-        return
+    a = find_parents(a)
+    b = find_parents(b)
     if a < b:
         parents[b] = a # 루트 노드가 값이 작은 것이 상위라고 가정하기
     else:
         parents[a] = b
+
 for _ in range(m):
     op, a, b = map(int, input().split())
     if op == 0: # 합치기
         union(a, b)
     else: # 동일 집합인지 판단
-        if find(a) == find(b):
+        if find_parents(a) == find_parents(b):
             print('YES')
         else:
             print('NO')
